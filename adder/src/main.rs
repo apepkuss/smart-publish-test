c7c8f07b19833d763660bd29283a11b03e990d3d
Xin Liu <sam@secondstate.io>
2022-10-11 17:21:10 +0800

    update adder
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- adder/src/main.rs
+++ adder/src/main.rs
@@ -1,6 +1,9 @@
-use add_one;
+use smart_add_one;
 
 fn main() {
     let num = 10;
-    println!("Hello, world! {num} plus one is {}!", add_one::add_one(num));
+    println!(
+        "Hello, world! {num} plus one is {}!",
+        smart_add_one::add_one(num)
+    );
 }
