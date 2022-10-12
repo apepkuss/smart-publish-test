b2dce67a9a30660f76b9b96fda4069522fcaf7ba
Xin Liu <sam@secondstate.io>
2022-10-12 20:27:34 +0800

    refactor!: Renamed print func to plus_one
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- adder/src/main.rs
+++ adder/src/main.rs
@@ -2,10 +2,10 @@ use smart_add_one_new;
 
 fn main() {
     let num = 10;
-    print(num);
+    plus_one(num);
 }
 
-fn print(num: i32) {
+fn plus_one(num: i32) {
     let plus_one = smart_add_one_new::add_one(num);
     println!("Hello, world! {num} plus one is {}!", echo(plus_one),);
 
