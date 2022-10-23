1297794a0914cfdfc01da61405a8fe6edcfa1a34
Xin Liu <sam@secondstate.io>
2022-10-23 21:13:29 +0800

    refactor!: rename plus to plus_num
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- adder/src/main.rs
+++ adder/src/main.rs
@@ -2,10 +2,10 @@ use smart_add_one_new;
 
 fn main() {
     let num = 10;
-    plus(num);
+    plus_num(num);
 }
 
-fn plus(num: i32) {
+fn plus_num(num: i32) {
     let plus_one = smart_add_one_new::add_one(num);
     println!("Hello, world! {num} plus one is {}!", echo(plus_one),);
 
